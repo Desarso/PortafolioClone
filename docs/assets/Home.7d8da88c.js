528b301f22550c0c0efe2110aca67a420777fa03
Gabriel Malek <malek.gabriel33@gmail.com>
2022-11-19 14:41:19 -0800

    all

---
+++ docs/assets/Home.7d8da88c.js
@@ -0,0 +1,1 @@
+import{t as e}from"./index.e186d939.js";const t=e('<div id="home"><section class="grid-container grid-cols-3 space-y-2"></section></div>');function i({}){return t.cloneNode(!0)}export{i as default};
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,50 +1,50 @@
-import eattherich from "../images/eatTheRich.webp";
-import masterclass from "../images/MasterClass.jpeg";
-import levelUp from "../images/levelUp.png";
-import letsPlay from "../images/letsPlay.gif";
-import catWalkGif from "../images/catWalk.gif";
-import starWars from "../images/starWars.gif";
-import whatIsSel from "../images/whatIsSel.jpg";
-import textingDriving from "../images/textingDriving.png";
-import digitalCitizenship from "../images/digitalCitizenship.jpeg";
-import diamondCat from "../images/diamondCat.gif";
-import city from "../images/city.webp";
-import tipsAdviceForFamilies from "../images/tipsAdviceForFamilies.webp";
-import figures from "../images/figures.webp";
+// import eattherich from "../images/eatTheRich.webp";
+// import masterclass from "../images/MasterClass.jpeg";
+// import levelUp from "../images/levelUp.png";
+// import letsPlay from "../images/letsPlay.gif";
+// import catWalkGif from "../images/catWalk.gif";
+// import starWars from "../images/starWars.gif";
+// import whatIsSel from "../images/whatIsSel.jpg";
+// import textingDriving from "../images/textingDriving.png";
+// import digitalCitizenship from "../images/digitalCitizenship.jpeg";
+// import diamondCat from "../images/diamondCat.gif";
+// import city from "../images/city.webp";
+// import tipsAdviceForFamilies from "../images/tipsAdviceForFamilies.webp";
+// import figures from "../images/figures.webp";
 import { For } from "solid-js";
 
 
 
 type Props = {};
 
-const images = [
-  eattherich,
-  masterclass,
-  levelUp,
-  letsPlay,
-  catWalkGif,
-  starWars,
-  whatIsSel,
-  textingDriving,
-  digitalCitizenship,
-  diamondCat,
-  city,
-  tipsAdviceForFamilies,
-  figures
-];
+// const images = [
+//   eattherich,
+//   masterclass,
+//   levelUp,
+//   letsPlay,
+//   catWalkGif,
+//   starWars,
+//   whatIsSel,
+//   textingDriving,
+//   digitalCitizenship,
+//   diamondCat,
+//   city,
+//   tipsAdviceForFamilies,
+//   figures
+// ];
 
 function Home({}: Props) {
   return (
     <div id="home">
       <section class="grid-container grid-cols-3 space-y-2">
-        <For each={images}>
+        {/* <For each={images}>
             {(image) => (
                <div class="image-container">
                <img src={image} alt="" />
                <div class="overlay"></div>
              </div>
             )}
-        </For>
+        </For> */}
       </section>
     </div>
   );
